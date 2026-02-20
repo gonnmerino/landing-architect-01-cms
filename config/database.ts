0e16d1cd6b40ef52c49e7501f76b64c241f0930a
gonnmerino <gonnmerino@gmail.com>
2026-02-19 21:53:49 -0300

    Configure Postgres SSL for Render deployment

--- config/database.ts
+++ config/database.ts
@@ -23,24 +23,34 @@ export default ({ env }) => {
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+      connection: env('DATABASE_URL')
+        ? {
+          connectionString: env('DATABASE_URL'),
+          ssl: env.bool('DATABASE_SSL', true) && {
+            rejectUnauthorized: env.bool(
+              'DATABASE_SSL_REJECT_UNAUTHORIZED',
+              false
+            ),
+          },
+        }
+        : {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'landing_architect_01_db'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'admin123'),
+          ssl: env.bool('DATABASE_SSL', false) && {
+            rejectUnauthorized: env.bool(
+              'DATABASE_SSL_REJECT_UNAUTHORIZED',
+              true
+            ),
+          },
+          schema: env('DATABASE_SCHEMA', 'public'),
         },
-        schema: env('DATABASE_SCHEMA', 'public'),
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     sqlite: {
       connection: {
